556729dcf4340394f70840dfe839fb660f5b05f2
Thiện Nguyễn <girlisbinarycode000@gmail.com>
2018-03-05 15:34:23 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+//MODULE IMPORT
+const express = require('express');
+const hbs = require('hbs');
+
+//SET UP FOR APP
+const app = express();
+// VIEW ENGINE
+app.set('view engine', 'hbs');
+
+//MIDDLE WARE
+app.use((req, res, next) => {
+  res.render('maintenace.hbs');
+});
+
+//PUBLIC FOLDER
+app.use(express.static(__dirname + '/public'));
+
+//REGISTER Partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//REGISTER HELPER
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+// GET METHOD
+app.get('/',(req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+
+  });
+});
+
+// GET METHOD
+app.get('/about',( req, res ) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'About Thien Dep Trai',
+  });
+});
+
+//LISTEN
+app.listen(3000,() => {
+  console.log('Server is running at port 3000');
+});
